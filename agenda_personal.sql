c9d89158227f74f1aabb4c93d44dbba09792a050
JavierGomez0 <jgomez-2024026@kinal.edu.gt>
2025-08-20 11:59:45 -0600

    Add files via upload
    La base de datos para la AgendaPersonal

---
+++ agenda_personal.sql
@@ -0,0 +1,21 @@
+drop database if exists agenda_personal;
+create database agenda_personal;
+use agenda_personal;
+ 
+create table Contactos(
+	codigoContactos integer auto_increment,
+    nombre varchar (64),
+    apellido varchar (64),
+    telefono varchar (16),
+    correo varchar (128),
+    constraint pk_contacto primary key (codigoContactos)
+);
+ 
+ 
+insert into Contactos (nombre, apellido, telefono, correo)
+values
+('Carlos', 'Ramírez', '5551-2345', 'carlos.ramirez@example.com'),
+('Ana', 'González', '5551-6789', 'ana.gonzalez@example.com'),
+('Luis', 'Mendoza', '5552-9876', 'luis.mendoza@example.com');
+ 
+select * from Contactos;
