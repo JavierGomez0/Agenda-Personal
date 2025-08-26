b9acb3bbe67e53dffbc1f4d2bdcec32176f4e228
klopez-2024401 <klopez-2024401@kinal.edu.gt>
2025-08-26 08:31:45 -0600

    se agregagron nuevos cambios

---
+++ src/main/java/org/nexus/Agenda_Personal/RegistroContactoWeb.java
@@ -0,0 +1,12 @@
+package org.nexus.Agenda_Personal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroContactoWeb {
+    public static void main(String[] args) {
+        SpringApplication.run(RegistroContactoWeb.class,args);
+    }
+
+}
